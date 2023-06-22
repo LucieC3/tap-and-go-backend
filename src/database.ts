34b48a5b6d4a6e535568b98729dc141ee11da7fd
Lucie Chauvet <lucie.chauvet10@gmail.com>
2023-06-22 18:35:10 +0200

    feat: edit database file

--- src/database.ts
+++ src/database.ts
@@ -1,14 +1,26 @@
 import { createConnection, Connection } from "mysql2/promise";
 
-let connection: Connection;
+let connection: Connection | undefined;
 
 export const connectToDB = async (): Promise<void> => {
-  connection = await createConnection({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
-  });
+  try {
+    connection = await createConnection({
+      host: process.env.DB_HOST,
+      user: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
+    });
+
+    console.log("Connected to the database");
+  } catch (error) {
+    console.error("Failed to connect to the database:", error);
+  }
 };
 
-export default connection;
+export const getDBConnection = (): Connection => {
+  if (!connection) {
+    throw new Error("Database connection has not been established");
+  }
+
+  return connection;
+};
