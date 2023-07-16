594c5cf48cccb10bd00084c1c7c2c0100f323799
Lucie Chauvet <lucie.chauvet10@gmail.com>
2023-07-16 19:37:51 +0200

    fix: station model

--- src/models/Station.ts
+++ src/models/Station.ts
@@ -1,18 +1,14 @@
-export interface Station {
-  stationId: number;
-  name: string;
-  address: string;
-  position: {
-    latitude: number;
-    longitude: number;
-  };
-  banking: boolean;
-  status: string;
-  totalStands: {
-    availabilities: {
-      bikes: number;
-      stands: number;
-    };
-    capacity: number;
-  };
+import { RowDataPacket } from "mysql2/promise";
+
+export interface Station extends RowDataPacket {
+  station_id: number;
+  station_name: string;
+  station_address: string;
+  station_latitude: number;
+  station_longitude: number;
+  station_banking: boolean;
+  station_status: string;
+  station_availabilities_bikes: number;
+  station_availabilities_stands: number;
+  station_capacity: number;
 }
