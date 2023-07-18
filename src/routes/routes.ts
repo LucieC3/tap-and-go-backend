d8f15863968c5d7f55efd0bf9be6f16b778dd551
Lucie Chauvet <lucie.chauvet10@gmail.com>
2023-07-18 14:46:25 +0200

    chore: rename routes file

--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -15,29 +15,17 @@ import { getStations } from "../controllers/StationController";
 
 const router = Router();
 
-// Route pour créer un utilisateur
 router.post("/register", createUserHandler);
-
-// Route pour se connecter
 router.post("/login", loginUser);
-
-// Route pour se déconnecter
 router.get("/logout", authenticateToken, logoutUser);
 
-// Route pour obtenir le profil de l'utilisateur
 router.get("/profile", authenticateToken, getUserProfileHandler);
 
 router.get("/stations/:id", getStations);
 
 router.get("/favorites", authenticateToken, getAllFavorites);
-
-// Route pour ajouter une station aux favoris de l'utilisateur
 router.post("/favorites/:stationId", authenticateToken, addFavorite);
-
-// Route pour supprimer une station des favoris de l'utilisateur
 router.delete("/favorites/:favoriteId", authenticateToken, removeFavorite);
-
-// Route pour obtenir les favoris de l'utilisateur
 router.get("/favorites", authenticateToken, getAllFavorites);
 
 export default router;
